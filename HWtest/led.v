538fc2f7badf6227f14ebb2be5a5ecf52b61b31f
hge2020 <hge2020@korea.ac.kr>
2021-11-18 17:17:58 +0900

    tb to top_module

---
+++ HWtest/led.v
@@ -0,0 +1,14 @@
+module led (clk, led_out);
+
+input clk;
+output reg [7:0] led_out;
+
+initial begin
+    led_out = 8'b0000_0000;
+end
+
+always @(negedge clk) begin
+    led_out = led_out + 1'b1;
+end
+
+endmodule

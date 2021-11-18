40ec325091337e5082bb422aba0f40ef44b7ff45
hge2020 <hge2020@korea.ac.kr>
2021-11-18 17:58:10 +0900

    bcd code with encoder

--- HWtest/tb_BCD_to_7segment.v
+++ HWtest/tb_BCD_to_7segment.v
@@ -1,20 +1,8 @@
 module tb_BCD_to_7segment;
 
-    reg D3, D2, D1, D0;
-    wire A, B, C, D, F, G;
+    input D9, D8, D7, D6, D5, D4, D3, D2, D1, D0;
+    output A, B, C, D, F, G;
 
-    BCD_to_7segment BCD7(D3,D2,D1,D0,A,B,C,D,E,F,G);
+    BCD_to_7segment BCD7(D9, D8, D7, D6, D5, D4, D3, D2, D1, D0,A,B,C,D,E,F,G);
 
-    initial begin
-        #10 {D3, D2, D1, D0} <= 4'b0000;
-        #10 {D3, D2, D1, D0} <= 4'b0001;
-        #10 {D3, D2, D1, D0} <= 4'b0010;
-        #10 {D3, D2, D1, D0} <= 4'b0011;
-        #10 {D3, D2, D1, D0} <= 4'b0100;
-        #10 {D3, D2, D1, D0} <= 4'b0101;
-        #10 {D3, D2, D1, D0} <= 4'b0110;
-        #10 {D3, D2, D1, D0} <= 4'b0111;
-        #10 {D3, D2, D1, D0} <= 4'b1000;
-        #10 {D3, D2, D1, D0} <= 4'b1001;
-    end
 endmodule

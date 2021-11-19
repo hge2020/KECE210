d56b80b629dafc742ad3ce0fa17bc8aafa92eb71
YUMJAE <ymj195@korea.ac.kr>
2021-11-19 15:19:27 +0900

    E 추가

--- HWtest/tb_BCD_to_7segment.v
+++ HWtest/tb_BCD_to_7segment.v
@@ -1,7 +1,7 @@
-module tb_BCD_to_7segment;
+module tb_BCD_to_7segment(D9, D8, D7, D6, D5, D4, D3, D2, D1, D0, A, B, C, D, E, F, G);
 
     input D9, D8, D7, D6, D5, D4, D3, D2, D1, D0;
-    output A, B, C, D, F, G;
+    output A, B, C, D, E, F, G;
 
     BCD_to_7segment BCD7(D9, D8, D7, D6, D5, D4, D3, D2, D1, D0,A,B,C,D,E,F,G);
 

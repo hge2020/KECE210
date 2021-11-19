205eb56f3b631bb197e16f7de05fd56c2423cb59
hge2020 <hge2020@korea.ac.kr>
2021-11-19 15:32:31 +0900

    ;

--- HWtest/tb_full_c_LED.v
+++ HWtest/tb_full_c_LED.v
@@ -1,5 +1,5 @@
 module tb_full_c_LED(
-input b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12
+input b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12;
 output  led_1_r, led_1_g, led_1_b,
         led_2_r, led_2_g, led_2_b,
         led_3_r, led_3_g, led_3_b,

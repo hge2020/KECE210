b0b2808b042113cf446c1bdfcbc9a66df59ddf0c
YUMJAE <ymj195@korea.ac.kr>
2021-12-02 18:35:57 +0900

    dj

--- rnd/5bit_LFSR.v
+++ rnd/5bit_LFSR.v
@@ -3,19 +3,22 @@ module LFSR (
     input rst,
     output reg [4:0] rnd
 );
-    reg [4:0] q;
+    reg [4:0] q, nxt_q;
     reg feedback;
 always @(posedge clk, negedge rst) begin
-    if (!rst) rnd <= 0;
+    if (!rst) rnd <= 5'b00000;
     else begin
-        q[4:1] <= q[3:0];
-        q[0] <= feedback;
+        // q[4:1] <= q[3:0];
+        // q[0] <= feedback;
+        q <= nxt_q;
     end
     rnd <= q;
 end
 
 always @(*) begin
-    feedback = q[2] ^ q[4];
+    //feedback = q[2] ^ q[4];
+    nxt_q = q << 1;
+    nxt_q[0] = q[2] ^ q[4];
 end
 
 endmodule

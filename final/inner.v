4036f2d38b81cab2ac00afdc0f83b12a10645446
hge2020 <hge2020@korea.ac.kr>
2021-12-03 23:54:58 +0900

    .

--- final/inner.v
+++ final/inner.v
@@ -1,121 +1,121 @@
 module turn (
-    input clk, rst,
-    input [4-1:0] keypad_in,
-    output reg en, whose
-);
-parameter turn1 = 1'b0;
-parameter turn2 = 1'b1;
+        input clk, rst,
+        input [4-1:0] keypad_in,
+        output reg en, whose
+    );
+    parameter turn1 = 1'b0;
+    parameter turn2 = 1'b1;
 
-reg finite_state;
+    reg finite_state;
 
-always @(posedge clk) begin
-    if (!rst) begin
-        finite_state <= turn1;
-    end
-end
-
-always @(posedge clk) begin
-    case(finite_state)
-    turn1: begin
-        if (keypad_in == 4'b0011) begin
-            finite_state <= turn2;
-            en <= 1'b1;
-            whose <= 1'b1;
-        end
-        else begin
+    always @(posedge clk) begin
+        if (!rst) begin
             finite_state <= turn1;
-            en <= 1'b0;
-            whose <= 1'b0;
         end
     end
-    turn2: begin
-        if (keypad_in == 4'b0001) begin
-            finite_state <= turn1;
-            en <= 1'b1;
-            whose <= 1'b0;
+
+    always @(posedge clk) begin
+        case(finite_state)
+        turn1: begin
+            if (keypad_in == 4'b0011) begin
+                finite_state <= turn2;
+                en <= 1'b1;
+                whose <= 1'b1;
+            end
+            else begin
+                finite_state <= turn1;
+                en <= 1'b0;
+                whose <= 1'b0;
+            end
         end
-        else begin
-            finite_state <= turn2;
-            en <= 1'b0;
-            whose <= 1'b1;
+        turn2: begin
+            if (keypad_in == 4'b0001) begin
+                finite_state <= turn1;
+                en <= 1'b1;
+                whose <= 1'b0;
+            end
+            else begin
+                finite_state <= turn2;
+                en <= 1'b0;
+                whose <= 1'b1;
+            end
         end
+        default: finite_state <= turn1;
+        endcase
     end
-    default: finite_state <= turn1;
-    endcase
-end
 
 endmodule
 
 
 
 module counter (
-    input clk, rst,
-    input en,
-    output reg [8-1:0] count
-);
-always @(posedge clk) begin
-    if (!rst) begin
-        count <= 8'b0;
+        input clk, rst,
+        input en,
+        output reg [8-1:0] count
+    );
+    always @(posedge clk) begin
+        if (!rst) begin
+            count <= 8'b0;
+        end
     end
-end
 
-always @(posedge en) begin //이거밖에 없어도 클락에서 안뜯어져서움직이나..?
-    count = count +1'b1; //아웃풋에ezr해도됨...?
-end
+    always @(posedge en) begin //이거밖에 없어도 클락에서 안뜯어져서움직이나..?
+        count = count +1'b1; //아웃풋에ezr해도됨...?
+    end
 
 endmodule
 
 
 
 module rand_gen (
-    input clk, rst,
-    input en,
-    output wire [5-1:0] rnd
-);
-    reg [5-1:0] q, nxt_q;
-    reg feedback;
+        input clk, rst,
+        input en,
+        output wire [5-1:0] rnd
+    );
+        reg [5-1:0] q, nxt_q;
+        reg feedback;
 
-assign rnd = q;
+    assign rnd = q;
 
-always @(posedge clk, negedge rst) begin
-    if (!rst) q <= 5'b11100;
-    else begin
-        q <= nxt_q;
+    always @(posedge clk, negedge rst) begin
+        if (!rst) q <= 5'b11100;
+        else begin
+            q <= nxt_q;
+        end
     end
-end
 
-always @(*) begin
-    nxt_q = q << 1;
-    nxt_q[0] = q[2] ^ q[4];
-end
+    always @(*) begin
+        nxt_q = q << 1;
+        nxt_q[0] = q[2] ^ q[4];
+    end
 
 endmodule
 
 
 
 module card_value (
-    input clk, rst,
-    input [5-1:0] rnd
-    output [2-1:0] color,
-    output [3-1:0] number
-);
-    reg [2-1:0] r_color;
-    reg [3-1:0] r_number;
+        input clk, rst,
+        input [5-1:0] rnd
+        output [2-1:0] color,
+        output [3-1:0] number
+    );
+        reg [2-1:0] r_color;
+        reg [3-1:0] r_number;
 
-assign color = r_color;
-assign number = r_number;
+    assign color = r_color;
+    assign number = r_number;
 
-always @(posedge clk) begin
-    if (!rst) begin
-        r_color <= 2'b0;
-        r_number <= 3'b0;
+    always @(posedge clk) begin
+        if (!rst) begin
+            r_color <= 2'b0;
+            r_number <= 3'b0;
+        end
     end
-end
 
-always @(*) begin
-    r_color <= (rnd[4:3] %3) + 1'b1;
-    r_number <= rnd[2:0] %5;
-end
+    always @(*) begin
+        r_color <= (rnd[4:3] %3) + 1'b1;
+        r_number <= rnd[2:0] %5;
+    end
 
 endmodule
 

cd282dd1d7fcb41b302d9bab03af1fbf58ea6620
hge2020 <hge2020@korea.ac.kr>
2021-12-06 17:13:39 +0900

    dmux

--- final/top_Halli_Galli.v
+++ final/top_Halli_Galli.v
@@ -14,8 +14,8 @@ module top_Halli_Galli (
     wire turn_whose;
     wire nofcard;
     wire [5-1:0] randn;
-    wire [5-1:0] value_player1;
-    wire [5-1:0] value_player2;
+    wire [5-1:0] rand_player1, rand_player2;
+    wire [5-1:0] value_player1, value_player2;
     wire right;
     wire [2-1:0] whop;
     wire [16-1:0] score_weight;
@@ -32,9 +32,11 @@ counter card_count(.clk(clk), .rst(rst), .en(random_enable),
 .count(nofcard));
 rand_gen rgen(.clk(clk), .rst(rst), .en(random_enable),
 .rnd(randn));
-card_value value_p1 (.clk(clk), .rst(rst),
+demux dmux(.clk(clk), .rst(rst), .whose(turn_whose), .rnd(randn),
+.card_value1(rand_player1), .card_value2(rand_player2));
+card_value value_p1 (.clk(clk), .rst(rst), .rnd(rand_player1),
 .color(value_player1[5-1:3]), .number(value_player1[3-1:0]));
-card_value value_p2 (.clk(clk), .rst(rst),
+card_value value_p2 (.clk(clk), .rst(rst), .rnd(rand_player2),
 .color(value_player2[5-1:3]), .number(value_player2[3-1:0]));
 
 is_right right(.clk(clk), .rst(rst), keypad_in(key_scan),

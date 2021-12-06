9b17014682b387664b8841d4ab781e4849bf51be
YUMJAE <ymj195@korea.ac.kr>
2021-12-06 20:28:24 +0900

    tb 추가

---
+++ final/tb/tb_who_push.v
@@ -0,0 +1,27 @@
+module tb_who_push;
+    reg clk, rst;
+    reg [4-1:0] keypad_in;
+    wire savewho1, savewho2;
+
+    who_push wp1(
+        .clk(clk), .rst(rst), .keypad_in(keypad_in),
+        .savewho1(savewho1), .savewho2(savewho2)
+    );
+
+    initial begin
+        rst <= 1'b0;
+        clk <= 1'b1;
+        forever #1 clk <= ~clk;
+    end
+
+    initial begin
+        #5 rst <= 1'b1;
+        #5 keypad_in <= 4'b0111;
+        #5 keypad_in <= 4'b1001;
+        #5 rst <= 1'b0;
+        #5 rst <= 1'b1;
+        #5 keypad_in <= 4'b1001;
+        #5 keypad_in <= 4'b0111;
+        #5 rst <= 1'b0;
+    end
+endmodule

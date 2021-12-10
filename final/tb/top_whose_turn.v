0a47757189906c61635dea50838669820b49b1ff
YUMJAE <ymj195@korea.ac.kr>
2021-12-11 03:22:44 +0900

    .

--- final/tb/top_whose_turn.v
+++ final/tb/top_whose_turn.v
@@ -9,7 +9,8 @@ module top_turn (
 
 keypad_scan scn(.clk(clk), .rst(rst), .keypad_in(keypad_in), .scan_out(keypad_scan));
 turn tunr(.clk(clk), .rst(rst), .keypad_in(keypad_scan), .en(en), .whose(whose));
-rand_gen rgen(.clk(clk), .rst(rst), .en(en), .rnd({l3, l4, l5, l6, l7}));
+//rand_gen rgen(.clk(clk), .rst(rst), .en(en), .rnd({l3, l4, l5, l6, l7}));
+counter c1(.clk(clk), .rst(rst), .en(en), .finish(1'b0), .count(count));
 
 assign {l0, l1, l2} = {en, whose, 1'b1};
 

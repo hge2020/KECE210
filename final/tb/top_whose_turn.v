8b090f41a5a5039c523c83bee87c954773c77db4
YUMJAE <ymj195@korea.ac.kr>
2021-12-11 01:27:10 +0900

    1

--- final/tb/top_whose_turn.v
+++ final/tb/top_whose_turn.v
@@ -1,16 +1,16 @@
 module top_turn (
     input clk, rst,
     input [12-1:0] keypad_in,
-    output l0, l1, l2, l3, l4, l5, l6, l7;
+    output l0, l1, l2, l3, l4, l5, l6, l7
 );
-    wire [4-1:0] keypad_in,
-    wire en, whose
+    wire [4-1:0] keypad_scan;
+    wire en, whose;
     wire demmy;
 
 
-keypad_scan scn(.clk(clk), .rst(rst), .keypad_in(keypad_in), .scan_out(keypad_in), .valid(dummy));
-turn tunr(.clk(clk), .rst(rst), .keypad_in(keypad_in), .en(en), .whose(whose));
+keypad_scan scn(.clk(clk), .rst(rst), .keypad_in(keypad_in), .scan_out(keypad_scan), .valid(dummy));
+turn tunr(.clk(clk), .rst(rst), .keypad_in(keypad_scan), .en(en), .whose(whose));
 
-assign {l0, l1, l2, l3, l4, l5, l6, l7} = {en, 6'b0, whose};
+assign {l0, l1, l2, l3, l4, l5, l6, l7} = {en, 1'b1, 1'b1, 1'b1, 1'b1, 1'b1, 1'b1, whose};
 
 endmodule

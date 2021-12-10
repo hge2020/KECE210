a3972ac1a46f036e2260c80534aafd664cacb7c7
hge2020 <hge2020@korea.ac.kr>
2021-12-10 23:02:58 +0900

    wtf

--- final/tb/top_output_seg.v
+++ final/tb/top_output_seg.v
@@ -4,11 +4,14 @@ module top_output_seg(
     output [8-1:0] seg_position
 );
 
-wire [14-1:0] seg;
+wire [7-1:0] seg1;
+wire [7-1:0] seg2;
+
+
 display dis1(.clk(clk), .rst(rst), .c_value(3'b001),
-.seg(seg[14-1:7]));
+.seg(seg1));
 display dis2(.clk(clk), .rst(rst), .c_value(3'b010),
-.seg(seg[7-1:0]));
-seven_segment seg_7(.clk(clk), .rst(rst), .seg1(seg[14-1:7]), .seg2(seg[7-1:0]),
+.seg(seg2));
+seven_segment seg_7(.clk(clk), .rst(rst), .seg1(seg1), .seg2(seg2),
 .data_out(seg_display), .data_pos(seg_position));
 endmodule

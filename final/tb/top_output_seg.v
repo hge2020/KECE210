38d7cef999afb2c9cf73a4c33ddd4fae64a51f35
hge2020 <hge2020@korea.ac.kr>
2021-12-10 22:35:34 +0900

    s

--- final/tb/top_output_seg.v
+++ final/tb/top_output_seg.v
@@ -9,6 +9,6 @@ display dis1(.clk(clk), .rst(rst), .c_value(3'b001),
 .seg(seg[14-1:7]));
 display dis2(.clk(clk), .rst(rst), .c_value(3'b010),
 .seg(seg[7-1:0]));
-seven_segment seg_7(.clk(clk), .rst(rst), .seg1(seg[14-1:7]),. seg2(seg[7-1:0]),
+seven_segment seg_7(.clk(clk), .rst(rst), .seg1(seg[14-1:7]), .seg2(seg[7-1:0]),
 .data_out(seg_display), .data_pos(seg_position));
 endmodule

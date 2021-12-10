4809558b0ae1bc1e5f3b5ee29e26a8c36fe32127
hge2020 <hge2020@korea.ac.kr>
2021-12-11 01:47:39 +0900

    topazz

--- final/tb/top_whose_turn.v
+++ final/tb/top_whose_turn.v
@@ -3,14 +3,14 @@ module top_turn (
     input [12-1:0] keypad_in,
     output l0, l1, l2, l3, l4, l5, l6, l7
 );
-    // wire [4-1:0] keypad_scan;
-    // wire en, whose;
-    // wire demmy;
+    wire [4-1:0] keypad_scan;
+    wire en, whose;
+    wire demmy;
 
 
-keypad_scan scn(.clk(clk), .rst(rst), .keypad_in(keypad_in), .scan_out({l3, l2, l1, l0}));
-// turn tunr(.clk(clk), .rst(rst), .keypad_in(keypad_scan), .en(en), .whose(whose));
+keypad_scan scn(.clk(clk), .rst(rst), .keypad_in(keypad_in), .scan_out(keypad_scan));
+turn tunr(.clk(clk), .rst(rst), .keypad_in(keypad_scan), .en(l0), .whose(l7));
 
-assign {l4, l5, l6, l7} = {1'b1, 1'b1, 1'b1, 1'b1};
+assign {l1, l2, l3, l4, l5, l6} = {1'b1, 1'b1, 1'b1, 1'b1, 1'b1, 1'b1};
 
 endmodule

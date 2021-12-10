50a609c26ba020fdc7d1ac7d9b77641744e08785
hge2020 <hge2020@korea.ac.kr>
2021-12-10 18:16:28 +0900

    하....

--- final/tb/top_output_seg.v
+++ final/tb/top_output_seg.v
@@ -18,9 +18,9 @@ always @(clk) begin
     end
 end
 
-display dis1(.clk(clk), .rst(rst), .c_value(value_player1[3-1:0]),
+display dis1(.clk(clk), .rst(rst), .c_value(3'b001),
 .seg(seg[14-1:7]));
-display dis2(.clk(clk), .rst(rst), .c_value(value_player2[3-1:0]),
+display dis2(.clk(clk), .rst(rst), .c_value(3'b010),
 .seg(seg[7-1:0]));
 seven_segment seg_7(.clk(clk), .rst(rst), .seg1(seg[14-1:7]),. seg2(seg[7-1:0]),
 .data_out(seg_display), .data_pos(seg_position));

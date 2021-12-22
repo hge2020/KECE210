81e9b26c09394516962d3ee477523ade8e3c5c13
YUMJAE <ymj195@korea.ac.kr>
2021-12-22 22:20:10 +0900

    커밋

--- final/inner_bell.v
+++ final/inner_bell.v
@@ -47,7 +47,7 @@ module who_push (     //검증완료
     reg [1:0] finite_state;
     //savewho1 [player1], savewho2 [player2]
 
-    always @(posedge clk) begin
+    always @(keypad_in) begin
         if (~rst) begin
             finite_state <= no_one;
             savewho1 <= 1'b0;
